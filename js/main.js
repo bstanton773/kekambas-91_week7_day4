796fbcb49de3cfd455960565374fa4ed3c27475a
Brian <bstanton773@gmail.com>
2022-06-16 13:40:36 -0500

    Add event listener on myHeader to change color on click

--- js/main.js
+++ js/main.js
@@ -26,11 +26,35 @@
     newHeader.id = 'myHeader';
     newHeader.className = 'text-center mt-3';
     newHeader.innerHTML = 'Created by Brian with the help of JavaScript';
+    newHeader.style.color = 'black';
 
 
     // Get the row of buttons
     let buttonRow = document.getElementsByClassName('row')[0];
     // Add the new header AFTER the row of buttons (as opposed to being appended as the last child of the row)
     buttonRow.after(newHeader);
-    
+
+}
+
+// Create a new scope
+
+{
+    // Get the header
+    let myHeader = document.getElementById('myHeader');
+    console.log(myHeader);
+
+    // Create a function to be executed when the event is triggered
+    function handleHeaderEvent(e){
+        // console.log(e)
+        let elementToChange = e.target;
+        if (elementToChange.style.color === 'black'){
+            elementToChange.style.color = 'red';
+        } else {
+            elementToChange.style.color = 'black'
+        }
+
+    }
+
+    // Add the handleHeaderEvent function to trigger when event happens to myHeader
+    myHeader.addEventListener('click', handleHeaderEvent)
 }
